7606d17adcda17f19452c5aa66490a2513764e6f
Zaidusyy <zaidsayyed07869@gmail.com>
2025-01-15 03:08:35 +0530

    half detials screen done

--- day2/lib/Details.dart
+++ day2/lib/Details.dart
@@ -21,24 +21,136 @@ class Details extends StatelessWidget {
       ),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 15),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Center(
-              child: Container(
-                margin: EdgeInsets.all(15),
-                height: 110,
-                width: 110,
+        child: Center(
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10),
+                height: 100,
+                width: 100,
                 alignment: Alignment.center,
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
+                    borderRadius: BorderRadius.circular(15),
                     color: Colors.blue,
                     image: DecorationImage(
-                        image: AssetImage('assets/images/Doctor2.jpg'),fit: BoxFit.cover)),
+                        image: AssetImage('assets/images/Doctor2.jpg'),
+                        fit: BoxFit.cover)),
               ),
-            )
-          ],
+              Text(
+                'Dr.Maria Tereza',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Cardiologist',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 16,
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.location_on_outlined,
+                    color: Colors.grey,
+                    size: 21,
+                  ),
+                  Text(
+                    'Viva Clinic ',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  Text(
+                    'Mykoly Lavrukhina St. 6, Kyiv',
+                    style: TextStyle(
+                        color: Colors.deepOrange,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                spacing: 10,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 75,
+                      padding: EdgeInsets.symmetric(horizontal: 10,vertical: 5),
+                      decoration: BoxDecoration(
+                          color: Colors.grey.withAlpha(40),
+                          borderRadius: BorderRadius.circular(15)),
+                      child: Center(
+                        child: Text(
+                          '10 years \n of experience',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      height: 75,
+                      padding: EdgeInsets.symmetric(horizontal: 10,vertical: 5),
+                      decoration: BoxDecoration(
+                          color: Colors.grey.withAlpha(40),
+                          borderRadius: BorderRadius.circular(15)),
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              spacing: 5,
+                              children: [
+                                Icon(
+                                  Icons.star,
+                                  color: Colors.deepOrange,
+                                  size: 25,
+                                ),
+                                Text(
+                                  '5.0',
+                                  style: TextStyle(
+                                      color: Colors.deepOrange,
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold),
+                                )
+                              ],
+                            ),
+                            Text(
+                              '22 Reviews',
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 14,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+            ],
+          ),
         ),
       ),
     );
