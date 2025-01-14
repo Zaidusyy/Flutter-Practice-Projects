86a85e6cc95aaf38502d6c98d92a63e4b7219efd
Zaidusyy <zaidsayyed07869@gmail.com>
2025-01-14 23:56:16 +0530

    profile pic

--- day2/lib/Details.dart
+++ day2/lib/Details.dart
@@ -8,6 +8,7 @@ class Details extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
+        backgroundColor: Colors.transparent,
         title: Text(
           'Details',
           style: TextStyle(
@@ -18,11 +19,28 @@ class Details extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: Padding(padding: EdgeInsets.symmetric(horizontal: 15),child: Column(
-        children: [
-
-        ],
-      ),),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Center(
+              child: Container(
+                margin: EdgeInsets.all(15),
+                height: 110,
+                width: 110,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                    color: Colors.blue,
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/Doctor2.jpg'),fit: BoxFit.cover)),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
