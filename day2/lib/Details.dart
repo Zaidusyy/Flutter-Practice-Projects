d06a8d1462389081fb21fcf97cd81c75756ff6c5
Zaidusyy <zaidsayyed07869@gmail.com>
2025-01-14 23:46:36 +0530

    detials works start

--- day2/lib/Details.dart
+++ day2/lib/Details.dart
@@ -7,7 +7,22 @@ class Details extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text(
+          'Details',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 25,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Padding(padding: EdgeInsets.symmetric(horizontal: 15),child: Column(
+        children: [
+
+        ],
+      ),),
     );
   }
 }
