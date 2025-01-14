790da127d689249a81904a7b9e06bef2c3697057
Zaidusyy <zaidsayyed07869@gmail.com>
2025-01-15 03:55:32 +0530

    90% done

--- day2/lib/Details.dart
+++ day2/lib/Details.dart
@@ -19,10 +19,12 @@ class Details extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 15),
-        child: Center(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
               Container(
                 margin: EdgeInsets.symmetric(vertical: 10),
@@ -87,7 +89,7 @@ class Details extends StatelessWidget {
                   Expanded(
                     child: Container(
                       height: 75,
-                      padding: EdgeInsets.symmetric(horizontal: 10,vertical: 5),
+                      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
                       decoration: BoxDecoration(
                           color: Colors.grey.withAlpha(40),
                           borderRadius: BorderRadius.circular(15)),
@@ -106,7 +108,7 @@ class Details extends StatelessWidget {
                   Expanded(
                     child: Container(
                       height: 75,
-                      padding: EdgeInsets.symmetric(horizontal: 10,vertical: 5),
+                      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
                       decoration: BoxDecoration(
                           color: Colors.grey.withAlpha(40),
                           borderRadius: BorderRadius.circular(15)),
@@ -148,11 +150,143 @@ class Details extends StatelessWidget {
                   ),
                 ],
               ),
-
+              SizedBox(
+                height: 20,
+              ),
+              Align(
+                alignment: AlignmentDirectional.centerStart,
+                child: Text(
+                  'About',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18),
+                  textAlign: TextAlign.start,
+                ),
+              ),
+              Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vitae lorem eros. Sed gravida dictum nulla. Fusce eget blandit lectus. Vestibulum ultricies tincidunt maximus. Quisque dictum lorem sem. Sed tristique vel metus eu varius. Nullam venenatis dolor eget erat egestas sodales. Aliquam aliquet nunc non aliquet sollicitudin. Pellentesque accumsan odio.',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 16,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                maxLines: 4,
+                textAlign: TextAlign.start,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    '22 Reviews',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18),
+                    textAlign: TextAlign.start,
+                  ),
+                  Spacer(),
+                  Text(
+                    'View all',
+                    style: TextStyle(color: Colors.deepOrange, fontSize: 16),
+                    textAlign: TextAlign.start,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              SizedBox(
+                height: 170,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 10,
+                    itemBuilder: (context, index) => Container(
+                          height: 170,
+                          width: 350,
+                          padding: EdgeInsets.all(15),
+                          margin: EdgeInsets.only(left: 10),
+                          decoration: BoxDecoration(
+                            color: Colors.grey.withAlpha(40),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: Column(
+                            children: [
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  CircleAvatar(
+                                    radius: 25,
+                                    backgroundColor: Colors.white,
+                                    backgroundImage:
+                                        AssetImage('assets/images/Doctor1.jpg'),
+                                  ),
+                                  Expanded(
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.start,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      spacing: 5,
+                                      children: [
+                                        Text(
+                                          'Julia J.',
+                                          style: TextStyle(
+                                              color: Colors.black,
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 20),
+                                        ),
+                                        Text(
+                                          '1 day ago',
+                                          style: TextStyle(
+                                              color: Colors.grey, fontSize: 16),
+                                        ),
+                                        Text(
+                                          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pulvinar dapibus lacus, ut vehicula velit porta et. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed vestibulum nisl id nisl sollicitudin, vel viverra tortor aliquet. Donec vitae tempor velit, ut dictum augue. Aliquam mattis risus ut.',
+                                          style: TextStyle(
+                                              color: Colors.grey,
+                                              fontSize: 16,
+                                              overflow: TextOverflow.ellipsis,
+                                              fontWeight: FontWeight.w500),
+                                          maxLines: 3,
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  _buildRating(),
+                                ],
+                              )
+                            ],
+                          ),
+                        )),
+              )
             ],
           ),
         ),
       ),
     );
   }
+
+  _buildRating() => SizedBox(
+        child: Row(
+          spacing: 5,
+          children: [
+            Icon(
+              Icons.star,
+              color: Colors.deepOrange,
+              size: 25,
+            ),
+            Text(
+              '5.0',
+              style: TextStyle(
+                  color: Colors.deepOrange,
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+      );
 }
